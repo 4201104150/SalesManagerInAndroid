6759cf9bdec9eea748ffc4504fb4434ab2049481
DESKTOP-00HTOC2\Escanor <shenlong436@gmail.com>
2018-10-19 19:33:59 +0700

    erros

--- ProjectSalesManager/app/src/main/java/com/example/escanor/util/Server.java
+++
@@ -1,8 +0,0 @@
-package com.example.escanor.util;
-
-public class Server
-{
-    public static String localhost="192.168.1.1";
-    public static String DuongDan="http://"+localhost+"/server/getMenu.php";
-
-}
